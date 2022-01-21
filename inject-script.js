4ae0a80d0165fdbd622dc36ba751b29fd90670d4
Chris Gustin <gustin.chris@gmail.com>
2022-01-21 17:30:07 -0500

    Working on feature to show total comments and contractors in notification center widget

--- inject-script.js
+++ inject-script.js
@@ -1,3 +1,5 @@
+/* --- Show notification count in title --- */
+
 // Listen for page load events, throttled to 2 seconds
 if (window.Pace) {
   let last_message_sent = Date.now()
@@ -22,4 +24,14 @@ document.addEventListener('click', (e) => {
   if (e.target.innerText == 'Mark all as read') {
     window.postMessage({type: 'UPDATE_NOTIFICATIONS'})
   }
+}, false)
+
+
+/* --- Show comment and contractor count in notifications widget --- */
+
+// TODO this works great, but it calls the inner function twice. Maybe add a throttle?
+document.querySelector('.icon-notifications-new-projects').addEventListener('click', () => {
+  document.addEventListener('DOMNodeInserted', (e) => {
+    console.log('Node inserted', document.querySelectorAll('cdbl-notification-item a').length)
+  }, { once: true })
 }, false)
